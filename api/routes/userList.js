517e47516fff5cedf59580c8cdfeafda16ac0e0f
Michael Ward <msward3@uncg.edu>
2018-09-19 22:24:07 -0400

    Piggybacked mon Mongoose commit prior

--- api/routes/userList.js
+++ api/routes/userList.js
@@ -16,6 +16,9 @@ router.get ('/', (req, res, next) => {
     .then(doc => {
         console.log(doc);
         res.status(200).json({doc});
+    }).catch (err => {
+        console.log(err);
+        res.status(500).json({error: err});
     })
    
     res.status(200).json({
