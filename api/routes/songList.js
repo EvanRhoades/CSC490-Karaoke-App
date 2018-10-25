cc0342616247e759766c07e96f4616d5a2f67f7f
Michael Ward <msward3@uncg.edu>
2018-10-25 13:33:54 -0400

    Testing

--- api/routes/songList.js
+++ api/routes/songList.js
@@ -8,18 +8,31 @@ const connection = new Sequelize( 'karaoke490', process.env.garbageman, process.
     dialect: 'mysql'
 });
 
+const Op = Sequelize.Op;
 var Song = connection.define('songtest', {
     Artist: {type: Sequelize.STRING, allowNull: false},
     Title: {type: Sequelize.STRING, allowNull: false},
-    dj_id: {type: Sequelize.INTEGER, allowNull: false}
+    dj_id: {type: Sequelize.INTEGER, allowNull: false},
+    loweredArtist: {type: Sequelize.STRING, allowNull: false},
+    loweredTitle: {type: Sequelize.STRING, allowNull: false}
 })
 
 
 //Handles GET request
-router.get ('/', (req, res, next) => {
+router.get ('/:artistSearch', (req, res, next) => {
+    const keyA = req.param.artistSearch;
     
+    var songs = Song.findAll({
+        where: {
+            loweredArtist: {
+                [Op.like]: '%'+ keyA +'%'
+            }
+        }
+    }).toJSON();
+
+
     res.status(200).json({
-        message: 'Test song title'
+        message: songs 
     });
 });
 
@@ -38,7 +51,9 @@ router.post ('/', (req, res, next) => {
         Song.create({
             Artist: req.body.Artist,
             Title: req.body.Title,
-            dj_id: parseInt(req.body.dj_id)        
+            dj_id: parseInt(req.body.dj_id),
+            loweredArtist: req.body.Artist.toLowerCase(),
+            loweredTitle: req.body.Title.toLowerCase()        
         })
     })
 
