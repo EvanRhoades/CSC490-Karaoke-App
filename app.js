0adc87d9343acee2f49cf6b7c89f31cab348289a
Michael Ward <msward3@uncg.edu>
2018-11-06 13:11:25 -0500

    GitHub asked to fix

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 //CORS Handling Freezes request as is; looking into if needed so commented out for now
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
